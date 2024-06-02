66fa8c43417dbc526315ec1eda7ed8f75a9a3e74
Julia Farias da Rocha <47614833+JuliaMoonCrystal@users.noreply.github.com>
2024-06-02 19:56:03 -0300

    testing the godot tool

--- Godot/Godot_Projects/player.gd
+++ Godot/Godot_Projects/player.gd
@@ -1,5 +1,6 @@
 extends Sprite2D
 
+@export var speed =100.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,5 +9,10 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(delta : float):
+	var input = Input.get_vector("ui_left","ui_right","ui_up","ui_down") # trocar a ordem do up e down também resolve o problema de movimentação
+	position.x =position.x+input.x * 5.0 # no Godot o espacamento tem que ser igual para todas as variaveis por padrão
+	position.y =position.y+input.y * 5.0
+	
+	#position += input * speed * delta# versão simplificada para fazer a movimentação
 	pass
